32f9d4231ea8023a44a8ec81cb3c59f98735a4cb
yzbkaka <yzbkaka@qq.com>
2019-07-22 17:49:05 +0800

    完成足球板块多轮的数据显示以及progressBar的使用

---
+++ app/src/main/java/com/example/yzbkaka/kakasports/Soccer/LinkActivity.java
@@ -0,0 +1,13 @@
+package com.example.yzbkaka.kakasports.Soccer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_link);
+    }
+}
